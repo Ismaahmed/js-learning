c83ba46251d7edad4f0a4c79da808f6639800908
Ismaahmed <muhiza53@gmail.com>
2026-02-25 14:21:45 +0300

    commit

--- app.js
+++ app.js
@@ -372,3 +372,75 @@
 // processStudents(ardayda)
 
 
+
+
+// let magaca = "Ismahan";
+// let da = 22;
+// let arday = true
+// console.log(magaca,  da,  arday)
+
+// let city = "mogdisho";
+// let students = 40;
+// let online = true;
+// let marks = [1, 2, 3, 4];
+
+// console.log(city, students, online, marks)
+
+// let  x = 20;
+
+// let  y = 10;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x > y);
+// console.log(x == y);
+
+// let score = 75;
+
+// if (score >= 50) {
+//     console.log("Waad gudubtay")
+// } else {
+//     console.log("Waad dhacday")
+// }
+
+//  let marks = 65; 
+//  if (marks >= 80){
+//     console.log("A")
+//  } else if (marks >=50) {
+//     console.log("B")
+
+//  } else{
+//     console.log("F")
+//  }
+
+// let age = 12;
+// if (age >= 18 && age <= 30){
+//     console.log("La aqbalay")
+// } 
+// else if (age <= 18 || age => 30) {
+//     console.log("Lama aqbalin")
+
+// }
+// let score = 45;
+// if (score >= 50 && score <= 100){
+//     console.log("Pass")
+// } else if (score < 50){
+//     console.log("fail")
+// }
+ 
+// let hour = 22;
+// if (hour >= 6 && hour <= 18 ){
+//     console.log("Maalin")
+// }else {
+//     console.log("Habeen")
+// }
+
+// let temp = 30;
+// if (temp > 25 && temp < 40){
+//     console.log("Kulul")
+// }else if (temp <= 25) {
+//     console.log("Qabow")
+
+// } else if(temp >= 40 ){
+//     console.log("Aad u kulul")
+// }
--- script.js
+++ script.js
@@ -1,70 +1,171 @@
-// let magaca = "Ismahan";
-// let da = 22;
-// let arday = true
-// console.log(magaca,  da,  arday)
 
-// let city = "mogdisho";
-// let students = 40;
-// let online = true;
-// let marks = [1, 2, 3, 4];
 
-// console.log(city, students, online, marks)
+// let x = 5;
+// let y = 10;
+// x = y;
+// y = 20;
+// console.log(x)
 
-// let  x = 20;
+// let number = 8;
 
-// let  y = 10;
-
-// console.log(x + y);
-// console.log(x - y);
-// console.log(x > y);
-// console.log(x == y);
-
-// let score = 75;
-
-// if (score >= 50) {
-//     console.log("Waad gudubtay")
-// } else {
-//     console.log("Waad dhacday")
+// if (number % 2  === 0){
+//     console.log("event")
+// }else{
+//     console.log("add")
 // }
 
-//  let marks = 65; 
-//  if (marks >= 80){
+// let number = -5;
+
+// if (number > 0){
+//     console.log("positive")
+// } else if (number < 0){
+//     console.log("nagative")
+// } else {
+//     console.log("zero")
+// }
+
+// let score = 85;
+// if (score >= 90){
 //     console.log("A")
-//  } else if (marks >=50) {
+// } else if (score >= 80){
 //     console.log("B")
 
-//  } else{
-//     console.log("F")
-//  }
+// }else if (score >= 70){
+//     console.log("C")
+// }else{
+//     console.log("fail")
+// }
 
-// let age = 12;
-// if (age >= 18 && age <= 30){
-//     console.log("La aqbalay")
-// } 
-// else if (age <= 18 || age => 30) {
-//     console.log("Lama aqbalin")
+
+// for (let i = 1; i < 20; i++){
+//       if (i  % 2 === 0){
+//         console.log(i ,"even");
+        
+//     } else{
+//         console.log(i ,"odd");
+        
+//     }
+// }
+  
+
+// for (let i = 1; i <30; i++){
+//     if (i % 3 === 0 && i % 5 === 0){
+//         console.log("FizzBuzz");
+//     } else if (i % 5 === 0){
+//         console.log("Buzz");
+        
+//     } else if (i % 3 === 0){
+//         console.log("Fizz")
+        
+//     } else{
+//         console.log(i);
+        
+//     }
+// }
+
+
+// for (let i = 1; i <50; i++){
+//     if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0){
+//         console.log("EvenFizzBuzz")
+
+//     } else if (i % 2 === 0 && i % 3 === 0){
+//         console.log("EvenFizz")
+//     } else if (i  % 2 === 0 && i % 5 === 0){
+//         console.log("EvenBuzz")
+//     } else if (i % 3 === 0 && i % 5 === 0){
+//         console.log("FizzBuzz")
+//     } else if (i % 2 === 0){
+//         console.log("even");
+        
+//     } else if (i % 3 === 0){
+//         console.log("Fizz");
+        
+//     } else if (i % 5 === 0){
+//         console.log("Buzz");
+        
+//     } else{
+//         console.log(i);
+        
+//     }
 
 // }
-let score = 45;
-if (score >= 50 && score <= 100){
-    console.log("Pass")
-} else if (score < 50){
-    console.log("fail")
-}
- 
-let hour = 22;
-if (hour >= 6 && hour <= 18 ){
-    console.log("Maalin")
-}else {
-    console.log("Habeen")
-}
 
-let temp = 30;
-if (temp > 25 && temp < 40){
-    console.log("Kulul")
-}else if (temp <= 25) {
-    console.log("Qabow")
+// for (let i = 1; i < 30; i++){
+//     let prime = (i === 2 || i === 3 ||i === 5 || i === 7 || i === 11 || i === 13 || i === 17 || i === 19 || i === 23 || i === 29  )
+//     if ( prime && i % 2 === 0 &&   i % 3 === 0 &&   i % 5 === 0 ){
+//         console.log("PrimeEvenFizzBuzz");
+        
 
-} else if(temp >= 40 ){
-    console.log("Aad u kulul")
+//     } else if ( prime && i % 2 === 0 &&  i % 3){
+//         console.log("PrimeEventFizz");
+        
+//     } else if ( prime && i % 3 === 0 &&  i % 5){
+//         console.log("PrimeFizzBuzz");
+        
+//     } else if ( prime && i % 3 === 0 &&   i % 5){
+//         console.log("EventFizzBuzz");
+        
+//     }  else if ( prime && i % 2 === 0){
+//         console.log("PrimeEvent");
+        
+//     }  else if ( prime && i % 3 === 0){
+//         console.log("PrimeFizz");
+        
+//     }  else if ( prime && i % 5 === 0){
+//         console.log("PrimeBuzz");
+        
+//     } else if (i % 2 === 0 && i % 3 === 0){
+//         console.log("EventFizz");
+        
+//     } else if (i % 2 === 0 && i % 5 === 0){
+//         console.log("EventBuzz");
+        
+//     } else if (i % 3 === 0 && i % 5 === 0){
+//         console.log("FizzBuzz");
+        
+//     } else if (i % 2 === 0 ){
+//         console.log("Event");
+        
+//     } else if (i % 3 === 0 ){
+//         console.log("Fizz");
+        
+//     }else if (i % 5 === 0 ){
+//         console.log("Buzz");
+        
+//     } else{
+//         console.log(i);
+        
+//     }
+// }
+
+
+for (let i = 1 ; i < 50; i++){
+    if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0){
+        console.log("mega");
+        
+    }else if (i % 2 === 0 && i % 3 === 0) {
+        console.log("EventFizz");
+        
+    } else if (i % 2 === 0 && i % 5 === 0){
+        console.log("EventBuzz");
+        
+    } else if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+        
+    } else if (i % 2 === 0){
+        console.log("Event");
+        
+
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+        
+        
+    } else if (i % 5 === 0){
+        console.log("Buzz");
+        
+    } else{
+        console.log(i);
+        
+    }
+
 }
