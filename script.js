1b5fbc4cc43b213bdc92bb9fb312af966bd5a016
Ismaahmed <muhiza53@gmail.com>
2026-02-26 13:57:24 +0300

    commit

--- script.js
+++ script.js
@@ -139,33 +139,178 @@
 // }
 
 
-for (let i = 1 ; i < 50; i++){
-    if (i % 2 === 0 && i % 3 === 0 && i % 5 === 0){
-        console.log("mega");
+// function evenOrOdd(EventOdd){
+//     if (EventOdd % 2 === 0){
+//         console.log("Event")
+
+//     } else{
+//         console.log("Odd");
         
-    }else if (i % 2 === 0 && i % 3 === 0) {
-        console.log("EventFizz");
+//     }
+// }
+// evenOrOdd(10)
+
+// function grade(score){
+//     if(score >= 90){
+//         console.log("A");
         
-    } else if (i % 2 === 0 && i % 5 === 0){
-        console.log("EventBuzz");
+//     } else if (score >= 80){
+//         console.log("B")
         
-    } else if (i % 3 === 0 && i % 5 === 0){
-        console.log("FizzBuzz");
+//     } else if (score >= 70) {
+//         console.log("C");
         
-    } else if (i % 2 === 0){
-        console.log("Event");
+//     } else {
+//         console.log("F");
+        
+//     }
+// }
+// grade(90)
+// grade(87)
+// grade(70)
+// grade(69)
+
+
+// let tartamayaasha = ["Axmed", "Hani", "Jaamac", "Leyla", "Cumar"];
+// let dhibcaha = [85, 45, 92, 30, 75];
+
+// for ( let i = 0 ; i < tartamayaasha.length; i++){
+//     let tartamaha = tartamayaasha[i]
+//     let dhibco = dhibcaha[i]
+  
+//     if (dhibco >= 90 ){
+//         console.log(`${tartamaha}: Geesi wuxuu helay Bilad Dahab ah`)
+//     } else if ( dhibco >= 70) {
+//         console.log(`${tartamaha}: Hambalyo wuxuu helay Bilad Qalin ah`);
+        
+//     } else if (dhibco > 50) {
+//         console.log(`${tartamaha}: wuu gudbay`);
+        
+//     } else {
+//         console.log(`${tartamaha}: Nasiib xumo wuu dhacay`);
+        
+//     }
+
+ 
+
+// }
+
+
+// let tartamayaasha2 = ["Axmed", "Hani", "Jaamac", "Leyla", "Cumar"];
+// let dhibcaha2 = [85, 45, 92, 30, 75];
+// let natijada = dhibcaha2[0]
+// let tartamaha2 = tartamayaasha2[0]
+
+//  for ( let i = 0 ; i < dhibcaha2.length; i++){
+  
+//     let dhibco2 = dhibcaha2[i]
+//     if (dhibco2 > natijada ){
+//         natijada = dhibco2
+//         tartamaha2 = tartamayaasha2[i]
+
+       
         
 
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-        
-        
-    } else if (i % 5 === 0){
-        console.log("Buzz");
-        
-    } else{
-        console.log(i);
-        
-    }
+//     }
+    
 
-}
+
+
+
+// }
+//  console.log(`Horyaalka waa:  ${tartamaha2} dhibcihiisuna waa ${natijada}`);
+
+// let tartamayaasha3 = ["Axmed", "Hani", "Jaamac", "Leyla", "Cumar"];
+// let dhibcaha3 = [85, 45, 92, 30, 75];
+// let gudbay = 0
+// let dhacay = 0
+// let wadarta = 0
+// for ( let i = 0 ; i < dhibcaha3.length; i++){
+ 
+//     wadarta += dhibcaha3[i]
+   
+//     if (dhibcaha3[i] >= 50) {
+//         gudbay ++;
+        
+
+//     } else{
+//         dhacay ++;
+        
+//     }
+
+    
+   
+    
+
+// }
+//  let celcelis = wadarta / dhibcaha3.length
+//  console.log(`Gudbay: ${gudbay}`);
+//  console.log(`Dhacay: ${dhacay}`);
+//  console.log(`Celcelis: ${celcelis}`);
+ 
+
+
+
+
+// let Number = [20, 3, 50, -5, 10]
+// let max = Number[0]
+// let min = Number[0]
+
+// for (let i = 1; i < Number.length; i++){
+//     let num = Number[i]
+//     if (num > max){
+//          max = num
+//     } 
+//      if (num < min) {
+//          min = num
+//     }
+       
+// }
+// console.log(max);
+// console.log(min);
+// let farqi = max - min
+
+
+// console.log(farqi);
+
+// let Number = [20, 3, 50, -5, 10];
+// let min1 = Infinity
+// let min2 = Infinity
+// for (let i = 0; i < numbers.length; i++){
+//     let num = numbers[i]
+//     if (num < min1){
+//          min2 = min1
+//          min1 = num
+//     } else if (num < min2 && num !== min1){
+//         min2 = num
+//     }
+       
+
+
+// }
+// console.log(min1);
+// console.log(min2);
+
+
+// let number = [20, 3, 50, -5, 10];
+// let max1 = -Infinity
+// let max2 = -Infinity
+// for (let i = 0; i < number.length; i++){
+//     let num = number[i]
+//     if (num > max1){
+//          max2 = max1
+//          max1 = num
+//     } else if (num > max2 && num !== max1){
+//         max2 = num
+//     }
+       
+
+
+// }
+// console.log(max1);
+// console.log(max2);
+console.log(NaN === NaN);
+
+
+
+
